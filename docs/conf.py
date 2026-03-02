a3b586d4070b584b359168cc17028306258f2139
Gorshkov Nikolay <nogamemorebrain@gmail.com>
2026-03-02 14:04:05 +0500

    fix: update readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -6,9 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import miggy
+
 project = 'miggy'
 copyright = '2025, kalombos'
 author = 'kalombos'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = miggy.__version__
+# The full version, including alpha/beta/rc tags.
+release = miggy.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
