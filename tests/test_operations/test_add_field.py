256b018ccb8584c003bc9ba44953c453bd24c6a9
Gorshkov Nikolay <nogamemorebrain@gmail.com>
2026-05-27 13:19:21 +0500

    add AddField test

--- tests/test_operations/test_add_field.py
+++ tests/test_operations/test_add_field.py
@@ -1,6 +1,6 @@
 import peewee as pw
 
-from miggy.operations import RemoveField
+from miggy.operations import AddField
 from miggy.schema import SchemaMigrator
 from miggy.state import State
 from tests.conftest import PatchedPgDatabase
@@ -10,19 +10,17 @@ def test_state_forwards() -> None:
 
     class User(pw.Model):
         name = pw.CharField()
-        email = pw.CharField()
 
     state = State({"user": User})
-    operation = RemoveField("user", "email")
+    operation = AddField("user", "email", pw.CharField())
     operation.state_forwards(state)
-    assert not hasattr(state["user"], "email")
+    assert isinstance(state["user"].email, pw.CharField)
 
 
 def test_database_forwards(patched_pg_db: PatchedPgDatabase) -> None:
 
     class User(pw.Model):
         name = pw.CharField()
-        email = pw.CharField()
 
         class Meta:
             database = patched_pg_db
@@ -30,10 +28,12 @@ def test_database_forwards(patched_pg_db: PatchedPgDatabase) -> None:
     User.create_table()
     patched_pg_db.clear_queries()
 
-    operation = RemoveField("user", "email")
-    from_state = State({"user": User})
+    to_state = State({"user": User})
 
-    operation.database_forwards(SchemaMigrator.from_database(patched_pg_db), from_state, State())[0].run()
+    operation = AddField("user", "email", pw.CharField(null=True))
+    operation.state_forwards(to_state)
 
-    queries = [q for q in patched_pg_db.queries if "ccu.constraint_name" not in q]
-    assert queries == ['ALTER TABLE "user" DROP COLUMN "email"']
+    operation.database_forwards(SchemaMigrator.from_database(patched_pg_db), State(), to_state)[0].run()
+
+    queries = patched_pg_db.queries
+    assert queries == ['ALTER TABLE "user" ADD COLUMN "email" VARCHAR(255)']
