4aca443ee07083d39ef43dbdf9620bb8317561fb
kalombo <nogamemorebrain@gmail.com>
2026-09-13 10:25:42 +0500

    update test

--- tests/test_operations/test_add_index.py
+++ tests/test_operations/test_add_index.py
@@ -16,50 +16,44 @@ def test_state_forwards() -> None:
         email = pw.CharField()
 
     state = State({"user": User})
-    operation = AddIndex("user", "name", "email", name="some_name")
+    operation = AddIndex("user", "name", "email", name="some_name", unique=True)
     operation.state_forwards(state)
 
     index = indexes_state(state["user"])["some_name"]
     assert index._name == "some_name"
     assert index._model is state["user"]
-    assert index._unique is False
-    assert index._expressions == (state["user"].name, state["user"].email)
+    assert index._unique
 
-
-def test_state_forwards_unique() -> None:
-    class User(pw.Model):
-        name = pw.CharField()
-
-    state = State({"user": User})
-    operation = AddIndex("user", "name", name="user_name", unique=True)
-    operation.state_forwards(state)
-
-    assert indexes_state(state["user"])["user_name"]._unique is True
+    assert [e.name for e in index._expressions] == ["name", "email"]
 
 
 @pytest.mark.parametrize(
-    ("index_params", "expected"),
+    ("fields", "kwargs", "expected"),
     [
-        ({"name": "user_name"}, 'CREATE INDEX "user_name" ON "user" ("name")'),
-        ({"name": "user_name", "unique": True}, 'CREATE UNIQUE INDEX "user_name" ON "user" ("name")'),
-        ({"name": "user_name", "safe": True}, 'CREATE INDEX IF NOT EXISTS "user_name" ON "user" ("name")'),
+        (("name",), {"name": "user_name"}, 'CREATE INDEX "user_name" ON "user" ("name")'),
+        (("name",), {"name": "user_name", "unique": True}, 'CREATE UNIQUE INDEX "user_name" ON "user" ("name")'),
+        (("name",), {"name": "user_name", "safe": True}, 'CREATE INDEX IF NOT EXISTS "user_name" ON "user" ("name")'),
         (
+            ("name",),
             {"name": "user_name", "concurrently": True},
             'CREATE INDEX CONCURRENTLY "user_name" ON "user" ("name")',
         ),
         (
+            ("name",),
             {"name": "user_name", "where": pw.SQL("name <> 'alice'")},
             """CREATE INDEX "user_name" ON "user" ("name") WHERE name <> 'alice'""",
         ),
         (
-            {"name": "user_name_created_at", "fields": ("name", "created_at")},
+            ("name", "created_at"),
+            {"name": "user_name_created_at"},
             'CREATE INDEX "user_name_created_at" ON "user" ("name", "created_at")',
         ),
     ],
 )
 def test_database_forwards(
     patched_pg_db: PatchedPgDatabase,
-    index_params: dict[str, Any],
+    fields: tuple[str, ...],
+    kwargs: dict[str, Any],
     expected: str,
 ) -> None:
     class User(pw.Model):
@@ -72,10 +66,8 @@ def test_database_forwards(
     User.create_table()
     patched_pg_db.clear_queries()
 
-    fields = index_params.pop("fields", ("name",))
-    name = index_params.pop("name")
     to_state = State({"user": User})
-    operation = AddIndex("user", *fields, name=name, **index_params)
+    operation = AddIndex("user", *fields, **kwargs)
     operation.state_forwards(to_state)
 
     operation.database_forwards(SchemaMigrator.from_database(patched_pg_db), State(), to_state)[0].run()
